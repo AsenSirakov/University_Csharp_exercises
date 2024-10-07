be2c20e96e17decc5b52c9311db885ea3e98e1d6
Asen Sirakov <asko2040@abv.bg>
2024-10-07 20:01:26 +0300

    Learning data types.

---
+++ First_Exercise/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter number a");
+int a = int.Parse(Console.ReadLine()); // Any integer will do
+
+Console.WriteLine("Enter number b");
+long b = int.Parse(Console.ReadLine()); // Has to be a long number
+
+Console.WriteLine("Enter string"); // String 
+string name = Console.ReadLine();
+
+bool isTrue = true;  // Bool will show True
+
+Console.WriteLine("Result of the inputs");
+
+Console.WriteLine($"The output is: {a}");
+
+Console.WriteLine($"The output is: {b}");
+
+Console.WriteLine($"The output is: {name}");
+
+Console.WriteLine($"The output is:{isTrue}");
